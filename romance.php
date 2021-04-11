1ca376533223b38d471c495d6675621876a14ed6
lorapokrajac <lora.pokrajac@gmail.com>
2021-04-11 11:34:22 -0700

    update romance.html to romance.php

--- romance.php
+++ romance.php
@@ -9,10 +9,20 @@
 
 <div class="header">	
 	<img src="logo.jpg" class="logo" alt="Movie Logo" width="70" height="70">
-	<div class="login-register">
-	<button class="login-button">Login</button>  
-	<button class="register-button"><a href = "register.html" > Register </a></button>
-	</div>
+	<?php 
+	session_start();
+	if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    	echo "<div class='login-register'>";
+		echo "<button class='logout-button'><a href = 'logout.html'>Logout</a></button>";
+		echo "</div>";
+	}
+	else {
+    	echo "<div class='login-register'>";
+		echo "<button class='login-button'><a href = 'login.html'>Login</a></button>"; 
+		echo "<button class='register-button'><a href = 'register.html' > Register </a></button>";
+		echo "</div>";
+	}
+	?>
 	<div class="search-container">
     <form action="/action_page.php">
       <input type="text" placeholder="Search" name="search">
@@ -21,15 +31,15 @@
   </div> 
 	
 	<div class="top-nav">
-  		<a href="home.html">Main Page</a>
-  		<a href="recentMovies.html" >Recent Movies</a>
-  		<a href="genre.html">Genre</a>	
-  		<a href="profile.html">Profile</a>
+  		<a href="home.php">Main Page</a>
+  		<a href="recentMovies.php" >Recent Movies</a>
+  		<a href="genre.php">Genre</a>	
+  		<a href="profile.php">Profile</a>
 	</div>
 	<div class="breadcrumb">
-		<a href="home.html">Home</a> >
-		<a href="genre.html">Genres</a> >
-		<a href="romance.html">Romance</a>
+		<a href="home.php">Home</a> >
+		<a href="genre.php">Genres</a> >
+		<a href="romance.php">Romance</a>
 		
 	</div>
 </div>
@@ -42,7 +52,7 @@
   			<div class="grid-item">
   				<h3>To All The Boys</h3>
   				<img src="allTheBoys.jpg" class="logo" alt="To All The Boys Movie Poster" width="215" height=300">
-  				<button class="posts"><a href = "allTheBoys.html" > See Posts </a></button>
+  				<button class="posts"><a href = "allTheBoys.php" > See Posts </a></button>
   			</div>
 		</div>			
 </div>
