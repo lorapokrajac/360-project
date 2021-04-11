4fd0121dbb7e2a5d2db5d14b1b370d73280eae16
lorapokrajac <lora.pokrajac@gmail.com>
2021-04-11 11:37:03 -0700

    update thriller.html to thriller.php

--- thriller.php
+++ thriller.php
@@ -9,10 +9,20 @@
 
 <div class="header">	
 	<img src="logo.jpg" class="logo" alt="Movie Logo" width="70" height="70">
-	<div class="login-register">
-	<button class="login-button">Login</button>  
-	<button class="register-button"><a href = "register.html" > Register </a></button>
-	</div>
+	<?php 
+	session_start();
+	if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    	echo "<div class='login-register'>";
+		echo "<button class='logout-button'><a href = 'logout.html'>Logout</a></button>";
+		echo "</div>";
+	}
+	else {
+    	echo "<div class='login-register'>";
+		echo "<button class='login-button'><a href = 'login.html'>Login</a></button>"; 
+		echo "<button class='register-button'><a href = 'register.html' > Register </a></button>";
+		echo "</div>";
+	}
+	?>
 	<div class="search-container">
     <form action="/action_page.php">
       <input type="text" placeholder="Search" name="search">
@@ -21,15 +31,15 @@
   </div> 
 	
 	<div class="top-nav">
-  		<a href="home.html">Main Page</a>
-  		<a href="recentMovies.html" >Recent Movies</a>
-  		<a href="genre.html" >Genre</a>	
-  		<a href="profile.html">Profile</a>
+  		<a href="home.php">Main Page</a>
+  		<a href="recentMovies.php" >Recent Movies</a>
+  		<a href="genre.php" >Genre</a>	
+  		<a href="profile.php">Profile</a>
 	</div>
 	<div class="breadcrumb">
-		<a href="home.html">Home</a> >
-		<a href="genre.html">Genres</a> >
-		<a href="thriller.html">Thrillers</a>
+		<a href="home.php">Home</a> >
+		<a href="genre.php">Genres</a> >
+		<a href="thriller.php">Thrillers</a>
 		
 		
 	</div>
@@ -43,17 +53,17 @@
   			<div class="grid-item">
   				<h3>Parasite</h3>
   				<img src="parasite.jpg" class="logo" alt="Parasite Movie Poster" width="215" height=300">
-  				<button class="posts"><a href = "parasite.html" > See Posts </a></button>
+  				<button class="posts"><a href = "parasite.php" > See Posts </a></button>
   			</div>
   			<div class="grid-item">
   				<h3>The Empty Man</h3>
  				<img src="empty.jpg" class="logo" alt="Palm Movie Poster" width="220" height=300">
-  				<button class="posts"> <a href = "empty.html" > See Posts </a></button>
+  				<button class="posts"> <a href = "empty.php" > See Posts </a></button>
   			</div>  
   			<div class="grid-item">
   				<h3>I Care A Lot</h3>
  				<img src="care.jpg" class="logo" alt="I Care A Lot Movie Poster" width="220" height=300">
-  				<button class="posts"> <a href = "care.html" > See Posts </a></button>
+  				<button class="posts"> <a href = "care.php" > See Posts </a></button>
   			</div>
   		</div>		
 </div>
