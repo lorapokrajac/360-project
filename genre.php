8074c2dec2b149ac3a9b0d09aa5921e476cece88
warner1334 <58993802+warner1334@users.noreply.github.com>
2021-04-12 16:18:22 -0700

    Update genre.php

--- genre.php
+++ genre.php
@@ -4,6 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="home.css">
 <script type="text/javascript" src="button.js"></script>
+
 </head>
 <body>
 
@@ -34,8 +35,12 @@
   		<a href="home.php">Main Page</a>
   		<a href="recentMovies.php" >Recent Movies</a>
   		<a href="genre.php" class="active">Genre</a>	
-  		<a href="profile.php">Profile</a>
-	</div>
+  		<?php
+		if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+			echo "<a href='profile.php'>Profile</a>";
+		}
+		?>
+		</div>
 	<div class="breadcrumb">
 		<a href="home.php">Home</a> >
 		<a href="genre.php">Genre</a>
@@ -48,51 +53,50 @@
 			<h2>Genres</h2>
 		</div>
 		<div class="grid-container">
-  		<div class="grid-item">
-  		<h3>Comedy</h3>
-  		<img src="yesDay.jpeg" class="logo" alt="Joker Movie Poster" width="215" height=300">
-  		<button class="all"><a href = "comedy.php" > See All </a></button>
-  		</div>
- 		<div class="grid-item">
- 		<h3>Action</h3>
- 	 	<img src="godzillaKong.jpeg" class="logo" alt="Joker Movie Poster" width="215" height=300">
-  		<button class="all"><a href = "action.php" > See All </a></button>
- 		</div>
-  		<div class="grid-item">
-  		<h3>Thriller</h3>
- 		<img src="parasite.jpg" class="logo" alt="Parasite Movie Poster" width="215" height=300">
-  		<button class="all"> <a href = "thriller.php" > See All </a></button>
-  		</div>  
-  		<div class="grid-item">
-  		<h3>Animated</h3>
- 		<img src="mulan.jpg" class="logo" alt="Mulan Movie Poster" width="220" height=300">
-  		<button class="all"><a href = "animated.php" > See All </a></button>
-  		</div>
-  		<div class="grid-item">
-  		<h3>Documentary</h3>
- 		<img src="kissGround.jpg" class="logo" alt="Kiss The Ground Movie Poster" width="215" height=300">
-  		<button class="all"> <a href = "documentary.php" > See All </a></button>
-  		</div>
-  		<div class="grid-item">
-  		<h3>Sci-Fi</h3>
- 		<img src="palm.jpg" class="logo" alt="Palm Springs Movie Poster" width="220" height=300">
-  		<button class="all"> <a href = "scifi.php" > See All </a></button>
-  		</div>  
-  		<div class="grid-item">
-  		<h3>Horror</h3>
- 		<img src="empty.jpg" class="logo" alt="Empty Man Movie Poster" width="220" height=300">
-  		<button class="all"> <a href = "horror.php" > See All </a></button>
-  		</div>  
-  		<div class="grid-item">
-  		<h3>Romance</h3>
- 		<img src="allTheBoys.jpg" class="logo" alt="To All The Boys Movie Poster" width="210" height=300">
-  		<button class="all"> <a href = "romance.php" > See All </a></button>
-  		</div>
-  		<div class="grid-item">
-  		<h3>Mystery</h3>
- 		<img src="enola.jpg" class="logo" alt="Enola Holmes Movie Poster" width="220" height=300">
-  		<button class="all"> <a href = "mystery.php" > See All </a></button>
-  		</div>
+		<?php
+	
+		$host = "localhost";
+		$database = "360_project";
+		$user = "webuser";
+		$password = "P@ssw0rd";
+
+		$connection = mysqli_connect($host, $user, $password, $database);
+
+		$error = mysqli_connect_error();
+		if($error != null)
+		{
+  			$output = "<p>Unable to connect to database!</p>";
+  			exit($output);
+		}
+		else
+		{
+    		$sql = "SELECT DISTINCT(genre) FROM movie;";
+	        $results = mysqli_query($connection, $sql);
+   		while ($row = mysqli_fetch_assoc($results)){
+			$genre = $row['genre'];
+			$sql2 = "SELECT title, poster FROM movie WHERE genre = '$genre';";
+			$results2 = mysqli_query($connection, $sql2);
+			echo "<div class='grid-item'>";
+			echo "<form action = 'genreSeeMore.php' method= 'post' id='$genre'>";
+  			echo "<h3>$genre</h3>";
+			while ($row2 = mysqli_fetch_assoc($results2)){
+				$poster = $row2['poster'];
+				$title = $row2['title'];
+  				echo "<img src= '$poster' class='logo' alt= '$title' width='215' height=300>";
+				break;
+			}
+			echo "<input type='hidden' value='$genre' name='genre' />";
+			echo "</form>";
+			echo "<button type='submit' form='$genre' value='Submit'>See more</button>";
+			echo "</div>";
+			
+		}
+
+  		mysqli_free_result($results);
+    		mysqli_close($connection);
+		}
+
+		?>
 </div>
 			
 </div>
