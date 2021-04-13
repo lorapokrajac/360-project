715efa936a5e8aad6ada12e44ce30a9e8981f423
warner1334 <58993802+warner1334@users.noreply.github.com>
2021-04-13 13:44:54 -0700

    Update recentMovies.php

--- recentMovies.php
+++ recentMovies.php
@@ -3,6 +3,7 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="home.css">
+<script type="text/javascript" src="button.js"></script>
 </head>
 <body>
 
@@ -10,10 +11,12 @@
 	<img src="logo.jpg" class="logo" alt="Movie Logo" width="70" height="70">
 	<?php 
 	session_start();
+	$login = false;
 	if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
     	echo "<div class='login-register'>";
 		echo "<button class='logout-button'><a href = 'logout.php'>Logout</a></button>";
 		echo "</div>";
+		$login = true;
 	}
 	else {
     	echo "<div class='login-register'>";
@@ -33,7 +36,11 @@
   		<a href="home.php">Main Page</a>
   		<a href="recentMovies.php" class="active">Recent Movies</a>
   		<a href="genre.php">Genre</a>
-  		<a href="profile.php">Profile</a>
+  		<?php 
+			if($login){
+  				echo "<a href='profile.php'>Profile</a>";
+			}
+		?>
 	</div>
 	<div class="breadcrumb">
 		<a href="home.php">Home</a> >
@@ -47,60 +54,52 @@
 			<h2>Recent Movies</h2>
 		</div>
 		<div class="grid-container">
-  		<div class="grid-item">
-  		<h3>The Joker</h3>
-  		<img src="joker.jpeg" class="logo" alt="Joker Movie Poster" width="220" height=300">
-  		<button class="save">Save</button>
-  		<button class="posts"> <a href = "joker.php" > See Posts </a></button>
-  		</div>
- 		<div class="grid-item">
- 		<h3>Parasite</h3>
- 		<img src="parasite.jpg" class="logo" alt="Parasite Movie Poster" width="220" height=300">
- 		<button class="save">Save</button>
-  		<button class="posts"> <a href = "parasite.php" > See Posts </a></button>
- 		</div>
-  		<div class="grid-item">
-  		<h3>Yes Day</h3>
- 		<img src="yesDay.jpeg" class="logo" alt="Yes Day Movie Poster" width="215" height=300">
- 		<button class="save">Save</button>
-  		<button class="posts"><a href = "yesDay.php" > See Posts </a></button>
-  		</div>  
-  		<div class="grid-item">
-  		<h3>I Care A Lot</h3>
- 		<img src="care.jpg" class="logo" alt="I Care A Lot Movie Poster" width="215" height=300">
- 		<button class="save">Save</button>
-  		<button class="posts"><a href = "iCareALot.php" > See Posts </a></button>
-  		</div>
-  		<div class="grid-item">
-  		<h3>Kiss The Ground</h3>
- 		<img src="kissGround.jpg" class="logo" alt="Kiss The Ground Movie Poster" width="215" height=300">
- 		<button class="save">Save</button>
-  		<button class="posts"> <a href = "kissTheGround.php" > See Posts </a></button>
-  		</div>
-  		<div class="grid-item">
-  		<h3>Soul</h3>
- 		<img src="soul.jpeg" class="logo" alt="Soul Movie Poster" width="220" height=300">
- 		<button class="save">Save</button>
-  		<button class="posts"> <a href = "soul.php" > See Posts </a></button>
-  		</div>  
-  		<div class="grid-item">
-  		<h3>Mulan</h3>
- 		<img src="mulan.jpg" class="logo" alt="Mulan Movie Poster" width="220" height=300">
- 		<button class="save">Save</button>
-  		<button class="posts"> <a href = "mulan.php" > See Posts </a></button>
-  		</div>  
-  		<div class="grid-item">
-  		<h3>Godzilla Vs. Kong</h3>
- 		<img src="godzillaKong.jpeg" class="logo" alt="Godzilla vs Kong Movie Poster" width="215" height=300">
- 		<button class="save">Save</button>
-  		<button class="posts"> <a href = "godzillaKong.php" > See Posts </a></button>
-  		</div>
-  		<div class="grid-item">
-  		<h3>Enola Holmes</h3>
- 		<img src="enola.jpg" class="logo" alt="Enola Holmes Movie Poster" width="220" height=300">
- 		<button class="save">Save</button>
-  		<button class="posts"> <a href = "enola.php" > See Posts </a></button>
-  		</div>
+		<?php
+		$host = "localhost";
+		$database = "360_project";
+		$user = "webuser";
+		$password = "P@ssw0rd";
+
+		$connection = mysqli_connect($host, $user, $password, $database);
+
+		$error = mysqli_connect_error();
+		if($error != null)
+		{
+  			$output = "<p>Unable to connect to database!</p>";
+  			exit($output);
+		}
+		else
+		{
+    		$sql = "SELECT title, poster, mid FROM movie ORDER BY mid DESC LIMIT 9";
+	        $results = mysqli_query($connection, $sql);
+		$count = 0;
+		while($row = mysqli_fetch_assoc($results)){
+  			$title = $row['title'];
+			$poster = $row['poster'];
+			echo   "<div class='grid-item'>";
+			echo   "<form action = 'movie.php' method = 'post' id='$title'>";
+  			echo	"<h3>$title</h3>";
+			if($count < 2)
+				{
+  				echo	"<img src='$poster' class='logo' alt= $title.'Poster' width='215' height=300>";
+  			}
+			else
+			{
+				echo	"<img src='$poster' class='logo' alt= $title.'Poster' width='220' height=300>";
+				$count = 0;
+			}
+			echo    "<input type='hidden' value='$title' name='title' />";
+			echo    "</form>";
+			echo    "<button type='submit' form='$title' value='Submit'>See more</button>";
+			echo 	"<button class='save'>Save</button>";
+			echo    "</div>";
+			$count++;     
+		}
+    		mysqli_free_result($results);
+    		mysqli_close($connection);
+		}
+		?>
+  		
 </div>
 			
 </div>
